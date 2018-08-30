cac8a70a8466632e74df1a3163adcdfd950cbdce
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-30 03:43:54 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,7 +54,7 @@ class Owner
   
   def sell_pets
      @pets.each do |type, pets|
-        @pets.map { |pets| pet.mood = "nervous"}
+        @pets.map { |pet| pet.mood = "nervous"}
     end
   end
   
